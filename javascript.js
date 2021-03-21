864fa39ea74c2e4e1d2799561fa97a5982d442c2
sonia casanova <soniacasanova@mbp-de-sonia-2.home>
2021-03-21 13:40:51 +0100

    again

--- javascript.js
+++ javascript.js
@@ -15,26 +15,37 @@ span.onclick = function () {
 };
 
 var quotes = [
-  'If you want to know the truth about something, there is a very good chance that Aries will give it to you straight.',
-  'You can definitely count on an Aries when the chips are down.',
-  'If you want to spark an Aries passion take their face in your hands, look them in the eye and passionately kiss them.',
-  'Aries is not about hanging in until the finish, or about being nice, they are born to initiate and inspire, and then to initiate and inspire again, and again.',
-  'Aries is born to make their mark on the world and take the road less traveled.',
-  'Aries are their own heroes and are true to themselves.',
-  'Aries is fully committed to what they believed in.',
-  'Aries is resilient, guileless and optimistic.',
-  'Failure is not an option for an Aries, it is a prerequisite for success.'
-  ];
-  var quotes1 = [
-      'Is this working?',
-      'please'
-      ];
-  
-  function newQuote() {
-      var randomNumber = Math.floor(Math.random() * (quotes.length));
-      document.getElementById('quoteDisplay').innerHTML = quotes [randomNumber];
-  }
-  window.addEventListener('load', newQuote, true);
+  [
+    "If you want to know the truth about something, there is a very good chance that Aries will give it to you straight.",
+    "You can definitely count on an Aries when the chips are down.",
+    "If you want to spark an Aries passion take their face in your hands, look them in the eye and passionately kiss them.",
+    "Aries is not about hanging in until the finish, or about being nice, they are born to initiate and inspire, and then to initiate and inspire again, and again.",
+    "Aries is born to make their mark on the world and take the road less traveled.",
+    "Aries are their own heroes and are true to themselves.",
+    "Aries is fully committed to what they believed in.",
+    "Aries is resilient, guileless and optimistic.",
+    "Failure is not an option for an Aries, it is a prerequisite for success.",
+  ],
 
-  
+  ["Is this working?", "please"],
 
+  ["yes!"],
+];
+
+document.getElementbyId("aries").addEventListener("click", function () {
+  var randomNumber = Math.floor(Math.random() * quotes[0].length);
+  console.log(quotes[0][randomNumber]);
+});
+document.getElementbyId("taurus").addEventListener("click", function () {
+  var randomNumber = Math.floor(Math.random() * quotes[1].length);
+  console.log(quotes[1][randomNumber]);
+});
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * quotes[0].length);
+  document.getElementById("aries").innerHTML = quotes[0][randomNumber];
+  var randomNumber = Math.floor(Math.random() * quotes[1].length);
+  document.getElementById("taurus").innerHTML = quotes[1][randomNumber];
+}
+
+window.addEventListener("load", newQuote, true);
